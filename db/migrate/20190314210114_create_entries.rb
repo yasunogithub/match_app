9e711bb21252c1ea6cba98ecfdb328dd883c16eb
Masahiro Nishimoto <Masahiro@Masahiro-no-MacBook-Pro.local>
2019-03-15 06:04:42 +0900

    add:entryモデル追加
    room、user、teacher各モデルを紐付けるentryモデル追加。

---
+++ db/migrate/20190314210114_create_entries.rb
@@ -0,0 +1,11 @@
+class CreateEntries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :entries do |t|
+      t.references :user, foreign_key: true, null: false
+      t.references :teacher, foreign_key: true, null: false
+      t.references :room, foreign_key: true, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_161733) do
+ActiveRecord::Schema.define(version: 2019_03_14_210114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entries", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "teacher_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_entries_on_room_id"
+    t.index ["teacher_id"], name: "index_entries_on_teacher_id"
+    t.index ["user_id"], name: "index_entries_on_user_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.bigint "user_id", default: 0, null: false
@@ -55,6 +66,9 @@ ActiveRecord::Schema.define(version: 2019_03_14_161733) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entries", "rooms"
+  add_foreign_key "entries", "teachers"
+  add_foreign_key "entries", "users"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "teachers"
   add_foreign_key "messages", "users"
